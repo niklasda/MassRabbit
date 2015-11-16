ac97b11d5fe86a8fd9884f89de2925b4230932e7
Niklas Dahlman <peda@danskebank.dk>
2015-11-16 15:57:04 +0100

    adding MVVM

---
+++ BusClient/ViewModels/MainViewModel.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Diagnostics;
+using BusMessages.Interfaces;
+using BusMessages.Requests;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using MassTransit;
+
+namespace BusClient.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            SendMessageCommand = new RelayCommand(SendMessage);
+        }
+
+        private string _textResult;
+        public string TextResult
+        {
+            get { return _textResult; }
+            set
+            {
+                if (_textResult != value)
+                {
+                    _textResult = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        public RelayCommand SendMessageCommand { get; set; }
+
+        private async void SendMessage()
+        {
+            IBusControl busControl = Bus.Factory.CreateUsingRabbitMq(x => x.Host(new Uri("rabbitmq://localhost"), h =>
+            {
+                h.Username("guest");
+                h.Password("guest");
+            }));
+
+            busControl.Start();
+
+            try
+            {
+                var serviceAddress = new Uri("rabbitmq://localhost/request_service");
+                var simpleClient = busControl.CreateRequestClient<ISimpleRequest, ISimpleResponse>(serviceAddress, TimeSpan.FromSeconds(10));
+                var complexClient = busControl.CreateRequestClient<IComplexRequest, ISimpleResponse>(serviceAddress, TimeSpan.FromSeconds(10));
+
+                ISimpleResponse simpleResponse = await simpleClient.Request(new SimpleRequest("1"));
+                ISimpleResponse complexResponse = await complexClient.Request(new ComplexRequest("2"));
+
+                Debug.WriteLine("Customer Name1: {0}", simpleResponse.CustomerName);
+                Debug.WriteLine("Customer Name2: {0}", complexResponse.CustomerName);
+                TextResult = simpleResponse.CustomerName + " - " + complexResponse.CustomerName;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception {0}", ex);
+            }
+            finally
+            {
+                Debug.WriteLine("Stopping it");
+                busControl.Stop();
+            }
+        }
+    }
+}
